83168b8575c88e1e6550d47aa6f4bea199451a36
cthibault <cthibault@egyde.ca>
2022-06-22 17:01:17 -0400

    Refactor terraform policies to add traceability

--- main/cloudforecast.tf
+++ main/cloudforecast.tf
@@ -1,101 +1,135 @@
+##################################
+# Definition of Cloudforecast role
+##################################
+
+data "aws_iam_policy_document" "cloudforecast-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::656227328603:root"]
+    }
+    condition {
+      test     = "StringEquals"
+      values   = [var.external_id]
+      variable = "sts:ExternalId"
+    }
+  }
+}
+
 resource "aws_iam_role" "cloudforecast" {
   name               = var.role_name
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "AWS": "arn:aws:iam::656227328603:root"
-      },
-      "Effect": "Allow",
-      "Condition": {
-        "StringEquals": {
-          "sts:ExternalId": var.external_id
-        }
-      }
-    }
-  ]
-} 
-EOF
+  assume_role_policy = data.aws_iam_policy_document.cloudforecast-role-policy.json
 }
 
-resource "aws_iam_role_policy" "cloudforecast_cur_policy" {
-  name = "CloudForecast-CUR-Policy"
-  role = aws_iam_role.cloudforecast.id
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "CostReadCUR",
-      "Action": [
-        "s3:Get*",
-        "s3:List*"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "arn:aws:s3:::${var.cur_bucket}",
-        "arn:aws:s3:::${var.cur_bucket}/*"
-      ]
-    }
-  ]
-} 
-EOF
+######################################################
+# Definition of policy on CUR for Cloudforecast role
+######################################################
+
+data "aws_iam_policy_document" "cloudforecast-cur-policy-doc" {
+  statement {
+    actions = [
+      "s3:Get*",
+      "s3:List*"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.cur_bucket}",
+      "arn:aws:s3:::${var.cur_bucket}/*"
+    ]
+  }
 }
 
-resource "aws_iam_role_policy" "costexplorer_policy" {
-  name = "CostExplorerPolicy"
-  role = aws_iam_role.cloudforecast.id
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-    "Sid": "CostExplorerAccess",
-    "Action": [
+resource "aws_iam_policy" "cloudforecast-cur-policy" {
+  name        = "CloudforecastCurPolicy"
+  description = "Access for Cloudforecast service on CUR S3 bucket"
+  policy = data.aws_iam_policy_document.cloudforecast-cur-policy-doc.json
+}
+
+resource "aws_iam_policy_attachment" "cloudforecast-cur-policy-attach" {
+  name       = "CloudforecastCurRolePolicy"
+  roles      = [aws_iam_role.cloudforecast.name]
+  policy_arn = aws_iam_policy.cloudforecast-cur-policy.arn
+}
+
+###############################################################
+# Definition of policy on Cost explorer for Cloudforecast role
+###############################################################
+
+data "aws_iam_policy_document" "cloudforecast-explorer-policy-doc" {
+  statement {
+    actions = [
       "ce:*"
-    ],
-    "Effect": "Allow",
-    "Resource": "*"
-  }]
-} 
-EOF
+    ]
+    resources = [
+      "*"
+    ]
+  }
 }
 
-resource "aws_iam_role_policy" "organization_policy" {
-  name = "OrganizationPolicy"
-  role = aws_iam_role.cloudforecast.id
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-    "Sid": "OrganizationListAccess",
-    "Action": [
+resource "aws_iam_policy" "cloudforecast-explorer-policy" {
+  name        = "CloudforecastExplorerPolicy"
+  description = "Access for Cloudforecast service on Cost explorer"
+  policy = data.aws_iam_policy_document.cloudforecast-explorer-policy-doc.json
+}
+
+resource "aws_iam_policy_attachment" "cloudforecast-explorer-policy-attach" {
+  name       = "CloudforecastExplorerRolePolicy"
+  roles      = [aws_iam_role.cloudforecast.name]
+  policy_arn = aws_iam_policy.cloudforecast-explorer-policy.arn
+}
+
+###############################################################
+# Definition of policy on Organization for Cloudforecast role
+###############################################################
+
+data "aws_iam_policy_document" "cloudforecast-organization-policy-doc" {
+  statement {
+    actions = [
       "organizations:ListAccounts"
-    ],
-    "Effect": "Allow",
-    "Resource": "*"
-  }]
-}
-EOF
+    ]
+    resources = [
+      "*"
+    ]
+  }
 }
 
-resource "aws_iam_role_policy" "savings_plan_policy" {
-  name = "SavingsPlansPolicy"
-  role = aws_iam_role.cloudforecast.id
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-    "Sid": "SavingsPlanAccess",
-    "Action": [
+resource "aws_iam_policy" "cloudforecast-organization-policy" {
+  name        = "CloudforecastOrganizationPolicy"
+  description = "Access for Cloudforecast service on Organization"
+  policy = data.aws_iam_policy_document.cloudforecast-organization-policy-doc.json
+}
+
+resource "aws_iam_policy_attachment" "cloudforecast-organization-policy-attach" {
+  name       = "CloudforecastOrganizationRolePolicy"
+  roles      = [aws_iam_role.cloudforecast.name]
+  policy_arn = aws_iam_policy.cloudforecast-organization-policy.arn
+}
+
+###############################################################
+# Definition of policy on Savings Plans for Cloudforecast role
+###############################################################
+
+data "aws_iam_policy_document" "cloudforecast-savings-policy-doc" {
+  statement {
+    actions = [
       "savingsplans:List*",
       "savingsplans:Describe*"
-    ],
-    "Effect": "Allow",
-    "Resource": "*"
-  }]
+    ]
+    resources = [
+      "*"
+    ]
+  }
 }
-EOF
+
+resource "aws_iam_policy" "cloudforecast-savings-policy" {
+  name        = "CloudforecastSavingsPlansPolicy"
+  description = "Access for Cloudforecast service on Savings Plans"
+  policy = data.aws_iam_policy_document.cloudforecast-savings-policy-doc.json
+}
+
+resource "aws_iam_policy_attachment" "cloudforecast-savings-policy-attach" {
+  name       = "CloudforecastSavingsPlansRolePolicy"
+  roles      = [aws_iam_role.cloudforecast.name]
+  policy_arn = aws_iam_policy.cloudforecast-savings-policy.arn
 }
